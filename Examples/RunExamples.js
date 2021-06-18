14570692aaa96244dbc6ab165112d09f66a2b7c8
Sergei Terentev <sergei.terentev@aspose.com>
2021-06-18 09:44:32 +0300

    Samples update for use with SDK v21.6

--- Examples/RunExamples.js
+++ Examples/RunExamples.js
@@ -10,7 +10,7 @@ global.serializer_1 = require("groupdocs-annotation-cloud/lib/serializer");
 
 //TODO: Get your ClientId and ClientSecret at https://dashboard.groupdocs.cloud (free registration is required).
 
-global.clientId = "XXXXXXXXXXXXXXXX";
+global.clientId = "XXXX-XXXX-XXXX-XXXX";
 global.clientSecret = "XXXXXXXXXXXXXXXX";
 
 global.myStorage = "First Storage";
@@ -35,31 +35,36 @@ global.storageApi = annotation_cloud.StorageApi.fromConfig(config);
 
 async function examples() {
 
-    // Uploading sample test files to storage
-    await require('./Utils').UploadTestFiles();
+    try {
+        // Uploading sample test files to storage
+        await require('./Utils').UploadTestFiles();
 
-    // Basic usage Examples
+        // Basic usage Examples
 
-    await require('./BasicUsage/GetSupportedFormats').Run();    
-    await require('./BasicUsage/GetDocumentInformation').Run();
+        await require('./BasicUsage/GetSupportedFormats').Run();    
+        await require('./BasicUsage/GetDocumentInformation').Run();
 
-    // Advanced usage Examples
-    await require('./AdvancedUsage/AddAnnotation/AddAreaAnnotation').Run();
-    //await require('./AdvancedUsage/AddAnnotation/AddDistanceAnnotation').Run();
-    await require('./AdvancedUsage/AddAnnotation/AddMultipleAnnotations').Run();
-    //await require('./AdvancedUsage/AddAnnotation/AddPointAnnotation').Run();
-    //await require('./AdvancedUsage/AddAnnotation/AddPolylineAnnotation').Run();
-    //await require('./AdvancedUsage/AddAnnotation/AddTextFieldAnnotation').Run();
-    //await require('./AdvancedUsage/AddAnnotation/AddTextRedactionAnnotation').Run();
-    //await require('./AdvancedUsage/AddAnnotation/AddTextReplacementAnnotation').Run();
-    //await require('./AdvancedUsage/AddAnnotation/AddTextStrikeoutAnnotation').Run();
-    //await require('./AdvancedUsage/AddAnnotation/AddTextUnderlineAnnotation').Run();
-    //await require('./AdvancedUsage/AddAnnotation/AddWatermarkAnnotation').Run();
+        // Advanced usage Examples
+        await require('./AdvancedUsage/AddAnnotation/AddAreaAnnotation').Run();
+        await require('./AdvancedUsage/AddAnnotation/AddDistanceAnnotation').Run();
+        await require('./AdvancedUsage/AddAnnotation/AddMultipleAnnotations').Run();
+        await require('./AdvancedUsage/AddAnnotation/AddPointAnnotation').Run();
+        await require('./AdvancedUsage/AddAnnotation/AddPolylineAnnotation').Run();
+        await require('./AdvancedUsage/AddAnnotation/AddTextFieldAnnotation').Run();
+        await require('./AdvancedUsage/AddAnnotation/AddTextRedactionAnnotation').Run();
+        await require('./AdvancedUsage/AddAnnotation/AddTextReplacementAnnotation').Run();
+        await require('./AdvancedUsage/AddAnnotation/AddTextStrikeoutAnnotation').Run();
+        await require('./AdvancedUsage/AddAnnotation/AddTextUnderlineAnnotation').Run();
+        await require('./AdvancedUsage/AddAnnotation/AddWatermarkAnnotation').Run();
 
-    await require('./AdvancedUsage/ExtractAnnotations/ExtractAnnotations').Run();
-    await require('./AdvancedUsage/DeleteAnnotations/DeleteAnnotations').Run();
-    await require('./AdvancedUsage/DocumentPreview/GetPages').Run();
-    await require('./AdvancedUsage/DocumentPreview/DeletePages').Run();
+        await require('./AdvancedUsage/ExtractAnnotations/ExtractAnnotations').Run();
+        await require('./AdvancedUsage/DeleteAnnotations/DeleteAnnotations').Run();
+        await require('./AdvancedUsage/DocumentPreview/GetPages').Run();
+        await require('./AdvancedUsage/DocumentPreview/DeletePages').Run();
+        
+    } catch (error) {
+        console.log(error.message)
+    }
 }
 
 examples();
